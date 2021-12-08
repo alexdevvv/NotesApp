0966f64847e8c8586a7b639ccc38e742a800b005
Aleksandr Sergeev <alex.dev.sergeev@gmail.com>
2021-12-08 21:49:50 +0300

    registration screen created

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+        val registrationScreen = RegistrationScreen()
+        ft.replace(R.id.fragment_container, registrationScreen).commit()
+
+
     }
 }
