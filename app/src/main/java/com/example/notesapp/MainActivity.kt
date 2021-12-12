f4673bf0779fd33ae729b4a3b8beaeec471e1994
Aleksandr Sergeev <alex.dev.sergeev@gmail.com>
2021-12-12 10:18:30 +0300

    add registrationFragment

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -2,18 +2,29 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.notesapp.databinding.ActivityMainBinding
 import com.example.notesapp.registration.registration_screen.RegistrationScreen
 
 class MainActivity : AppCompatActivity() {
+
+    private val registrationScreen = RegistrationScreen()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
-        val registrationScreen = RegistrationScreen()
-        ft.replace(R.id.fragment_container, registrationScreen).commit()
+        replaceFragment(registrationScreen)
 
     }
+
+   private fun replaceFragment(fragment: Fragment) {
+        val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, fragment).commit()
+    }
+
+
+
 }
--- app/src/main/java/com/example/notesapp/registration/registration_screen/RegistrationScreen.kt
+++ app/src/main/java/com/example/notesapp/registration/registration_screen/RegistrationScreen.kt
@@ -1,25 +1,30 @@
 package com.example.notesapp.registration.registration_screen
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.notesapp.R
+import com.example.notesapp.databinding.FragmentRegistrationScreenBinding
+import com.example.notesapp.registration.notes_screen.NotesScreen
 
 
-class RegistrationScreen : Fragment() {
+class RegistrationScreen : Fragment(R.layout.fragment_registration_screen) {
+    private val binding: FragmentRegistrationScreenBinding by viewBinding()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRegistrationButton()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_screen, container, false)
+
+    private fun initRegistrationButton() {
+        binding.registrationBt.setOnClickListener() {
+            val fragmentNotesScreen = NotesScreen()
+            val fragmentTransaction = activity?.supportFragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.fragment_container, fragmentNotesScreen)?.commit()
+        }
+
     }
 
 }
